8873bfad93e1bb1a983e090a02b40fbc5b486c14
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-07 14:37:58 +0700

    first commit

---
+++ application/logs/log-2022-12-07.php
@@ -0,0 +1,155 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-12-07 08:27:20 --> Config Class Initialized
+INFO - 2022-12-07 08:27:20 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:27:20 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:27:20 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:27:20 --> URI Class Initialized
+DEBUG - 2022-12-07 08:27:20 --> No URI present. Default controller set.
+INFO - 2022-12-07 08:27:20 --> Router Class Initialized
+INFO - 2022-12-07 08:27:20 --> Output Class Initialized
+INFO - 2022-12-07 08:27:20 --> Security Class Initialized
+DEBUG - 2022-12-07 08:27:20 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:27:20 --> Input Class Initialized
+INFO - 2022-12-07 08:27:20 --> Language Class Initialized
+INFO - 2022-12-07 08:27:20 --> Loader Class Initialized
+INFO - 2022-12-07 08:27:21 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:27:21 --> Controller Class Initialized
+INFO - 2022-12-07 08:27:21 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\welcome_message.php
+INFO - 2022-12-07 08:27:21 --> Final output sent to browser
+DEBUG - 2022-12-07 08:27:21 --> Total execution time: 0.2497
+INFO - 2022-12-07 08:32:57 --> Config Class Initialized
+INFO - 2022-12-07 08:32:57 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:32:57 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:32:57 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:32:57 --> URI Class Initialized
+INFO - 2022-12-07 08:32:57 --> Router Class Initialized
+INFO - 2022-12-07 08:32:57 --> Output Class Initialized
+INFO - 2022-12-07 08:32:57 --> Security Class Initialized
+DEBUG - 2022-12-07 08:32:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:32:57 --> Input Class Initialized
+INFO - 2022-12-07 08:32:57 --> Language Class Initialized
+INFO - 2022-12-07 08:32:57 --> Loader Class Initialized
+INFO - 2022-12-07 08:32:57 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:32:57 --> Database Driver Class Initialized
+ERROR - 2022-12-07 08:32:57 --> Severity: error --> Exception: Unknown database 'db_ekatalog' C:\xampp\htdocs\ekatalog\system\database\drivers\mysqli\mysqli_driver.php 201
+INFO - 2022-12-07 08:34:53 --> Config Class Initialized
+INFO - 2022-12-07 08:34:53 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:34:53 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:34:53 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:34:53 --> URI Class Initialized
+INFO - 2022-12-07 08:34:53 --> Router Class Initialized
+INFO - 2022-12-07 08:34:53 --> Output Class Initialized
+INFO - 2022-12-07 08:34:53 --> Security Class Initialized
+DEBUG - 2022-12-07 08:34:53 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:34:53 --> Input Class Initialized
+INFO - 2022-12-07 08:34:53 --> Language Class Initialized
+INFO - 2022-12-07 08:34:53 --> Loader Class Initialized
+INFO - 2022-12-07 08:34:53 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:34:53 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:34:53 --> Controller Class Initialized
+INFO - 2022-12-07 08:34:53 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\welcome_message.php
+INFO - 2022-12-07 08:34:53 --> Final output sent to browser
+DEBUG - 2022-12-07 08:34:53 --> Total execution time: 0.0196
+INFO - 2022-12-07 08:34:57 --> Config Class Initialized
+INFO - 2022-12-07 08:34:57 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:34:57 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:34:57 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:34:57 --> URI Class Initialized
+INFO - 2022-12-07 08:34:57 --> Router Class Initialized
+INFO - 2022-12-07 08:34:57 --> Output Class Initialized
+INFO - 2022-12-07 08:34:57 --> Security Class Initialized
+DEBUG - 2022-12-07 08:34:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:34:57 --> Input Class Initialized
+INFO - 2022-12-07 08:34:57 --> Language Class Initialized
+ERROR - 2022-12-07 08:34:57 --> 404 Page Not Found: Welcome/tampil
+INFO - 2022-12-07 08:35:10 --> Config Class Initialized
+INFO - 2022-12-07 08:35:10 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:35:10 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:35:10 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:35:10 --> URI Class Initialized
+INFO - 2022-12-07 08:35:10 --> Router Class Initialized
+INFO - 2022-12-07 08:35:10 --> Output Class Initialized
+INFO - 2022-12-07 08:35:10 --> Security Class Initialized
+DEBUG - 2022-12-07 08:35:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:35:10 --> Input Class Initialized
+INFO - 2022-12-07 08:35:10 --> Language Class Initialized
+INFO - 2022-12-07 08:35:10 --> Loader Class Initialized
+INFO - 2022-12-07 08:35:10 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:35:10 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:35:10 --> Controller Class Initialized
+INFO - 2022-12-07 08:35:10 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\hello.php
+INFO - 2022-12-07 08:35:10 --> Final output sent to browser
+DEBUG - 2022-12-07 08:35:10 --> Total execution time: 0.0171
+INFO - 2022-12-07 08:35:32 --> Config Class Initialized
+INFO - 2022-12-07 08:35:32 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:35:32 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:35:32 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:35:32 --> URI Class Initialized
+INFO - 2022-12-07 08:35:32 --> Router Class Initialized
+INFO - 2022-12-07 08:35:32 --> Output Class Initialized
+INFO - 2022-12-07 08:35:32 --> Security Class Initialized
+DEBUG - 2022-12-07 08:35:32 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:35:32 --> Input Class Initialized
+INFO - 2022-12-07 08:35:32 --> Language Class Initialized
+INFO - 2022-12-07 08:35:32 --> Loader Class Initialized
+INFO - 2022-12-07 08:35:32 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:35:32 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:35:32 --> Controller Class Initialized
+INFO - 2022-12-07 08:35:32 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\hello.php
+INFO - 2022-12-07 08:35:32 --> Final output sent to browser
+DEBUG - 2022-12-07 08:35:32 --> Total execution time: 0.0184
+INFO - 2022-12-07 08:36:28 --> Config Class Initialized
+INFO - 2022-12-07 08:36:28 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:36:28 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:36:28 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:36:28 --> URI Class Initialized
+INFO - 2022-12-07 08:36:28 --> Router Class Initialized
+INFO - 2022-12-07 08:36:28 --> Output Class Initialized
+INFO - 2022-12-07 08:36:28 --> Security Class Initialized
+DEBUG - 2022-12-07 08:36:28 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:36:28 --> Input Class Initialized
+INFO - 2022-12-07 08:36:28 --> Language Class Initialized
+INFO - 2022-12-07 08:36:28 --> Loader Class Initialized
+INFO - 2022-12-07 08:36:28 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:36:28 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:36:28 --> Controller Class Initialized
+INFO - 2022-12-07 08:36:28 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\hello.php
+INFO - 2022-12-07 08:36:28 --> Final output sent to browser
+DEBUG - 2022-12-07 08:36:28 --> Total execution time: 0.0180
+INFO - 2022-12-07 08:36:37 --> Config Class Initialized
+INFO - 2022-12-07 08:36:37 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:36:37 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:36:37 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:36:37 --> URI Class Initialized
+INFO - 2022-12-07 08:36:37 --> Router Class Initialized
+INFO - 2022-12-07 08:36:37 --> Output Class Initialized
+INFO - 2022-12-07 08:36:37 --> Security Class Initialized
+DEBUG - 2022-12-07 08:36:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:36:37 --> Input Class Initialized
+INFO - 2022-12-07 08:36:37 --> Language Class Initialized
+INFO - 2022-12-07 08:36:37 --> Loader Class Initialized
+INFO - 2022-12-07 08:36:37 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:36:37 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:36:37 --> Controller Class Initialized
+INFO - 2022-12-07 08:36:37 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\hello.php
+INFO - 2022-12-07 08:36:37 --> Final output sent to browser
+DEBUG - 2022-12-07 08:36:37 --> Total execution time: 0.0188
+INFO - 2022-12-07 08:36:41 --> Config Class Initialized
+INFO - 2022-12-07 08:36:41 --> Hooks Class Initialized
+DEBUG - 2022-12-07 08:36:41 --> UTF-8 Support Enabled
+INFO - 2022-12-07 08:36:41 --> Utf8 Class Initialized
+INFO - 2022-12-07 08:36:41 --> URI Class Initialized
+INFO - 2022-12-07 08:36:41 --> Router Class Initialized
+INFO - 2022-12-07 08:36:41 --> Output Class Initialized
+INFO - 2022-12-07 08:36:41 --> Security Class Initialized
+DEBUG - 2022-12-07 08:36:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-12-07 08:36:41 --> Input Class Initialized
+INFO - 2022-12-07 08:36:41 --> Language Class Initialized
+INFO - 2022-12-07 08:36:41 --> Loader Class Initialized
+INFO - 2022-12-07 08:36:41 --> Helper loaded: url_helper
+INFO - 2022-12-07 08:36:41 --> Database Driver Class Initialized
+INFO - 2022-12-07 08:36:41 --> Controller Class Initialized
+INFO - 2022-12-07 08:36:41 --> File loaded: C:\xampp\htdocs\ekatalog\application\views\hello.php
+INFO - 2022-12-07 08:36:41 --> Final output sent to browser
+DEBUG - 2022-12-07 08:36:41 --> Total execution time: 0.0357
---
+++ application/views/hello.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "halo $nama <br>";
+echo "nis = $nis <br>";
+echo "kata kata hari ini = $word <br>";
+
+?>
